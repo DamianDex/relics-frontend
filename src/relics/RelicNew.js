5e7415338806595975dc2e9fc8b320ef4df256f7
farad <damian.fara@pega.com>
2018-02-23 15:26:16 +0100

    Add possibility to select multiple categories during creation of relic

--- src/relics/RelicNew.js
+++ src/relics/RelicNew.js
@@ -1,15 +1,19 @@
 import React from "react";
-import axios from 'axios';
-import { Button, InputGroup, InputGroupAddon, Input } from 'reactstrap';
+import axios from "axios";
+import {Button, Form, FormGroup, Input, Label} from "reactstrap";
 
 class RelicNew extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            placeName: ""
+            placeName: "",
+            communeName: "",
+            categories: []
         };
 
         this.handlePlaceNameChange = this.handlePlaceNameChange.bind(this);
+        this.handleCommuneNameChange = this.handleCommuneNameChange.bind(this);
+        this.handleCategoryChange = this.handleCategoryChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
 
     }
@@ -18,22 +22,38 @@ class RelicNew extends React.Component {
         this.setState({placeName: e.target.value})
     }
 
+    handleCommuneNameChange(e) {
+        this.setState({communeName: e.target.value})
+    }
+
+    handleCategoryChange(e) {
+
+        var categories = [];
+        var options = e.target.options;
+
+        for (var i = 0, l = options.length; i < l; i++) {
+            if (options[i].selected) {
+                categories.push({categoryName: options[i].value});
+            }
+        }
+        this.setState({categories: categories});
+        console.log(categories);
+    }
+
     handleClick(e) {
         console.log("Clicked!!");
 
-        var var_placeName = this.state.placeName;
+        var placeName = this.state.placeName;
+        var communeName = this.state.communeName;
+        var categories = this.state.categories;
 
         axios.post('http://localhost:8080/api/relics', {
-            categories: [
-                {
-                    categoryName: "aaa"
-                }
-            ],
-            communeName: "string",
+            categories: categories,
+            communeName: communeName,
             districtName: "string",
             latitude: 0,
             longitude: 0,
-            placeName: var_placeName,
+            placeName: placeName,
             voivodeshipName: "string"
         }, {
             headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}
@@ -48,24 +68,29 @@ class RelicNew extends React.Component {
 
     render() {
         return (
-            <div>
-                <InputGroup size="sm">
-                    <InputGroupAddon addonType="prepend">Place Name</InputGroupAddon>
-                    <Input type="text" placeholder="Pass place name..." value={this.state.placeName} onChange={this.handlePlaceNameChange} />
-                </InputGroup>
-                <br/>
-                <InputGroup size="sm">
-                    <InputGroupAddon addonType="prepend">Commune Name</InputGroupAddon>
-                    <Input />
-                </InputGroup>
-                <br/>
-                <InputGroup size="sm">
-                    <InputGroupAddon addonType="prepend">District Name</InputGroupAddon>
-                    <Input />
-                </InputGroup>
-                <br/>
-                <Button color="primary" onClick={this.handleClick}>Create new Relic !</Button>
-            </div>
+            <Form>
+                <FormGroup>
+                    <Label>Place Name</Label>
+                    <Input type="text" placeholder="Place name" value={this.state.placeName}
+                           onChange={this.handlePlaceNameChange}/>
+                </FormGroup>
+                <FormGroup size="sm">
+                    <Label>Commune Name</Label>
+                    <Input type="text" placeholder="Commune name" value={this.state.communeName}
+                           onChange={this.handleCommuneNameChange}/>
+                </FormGroup>
+                <FormGroup>
+                    <Label>Categories</Label>
+                    <Input type="select" name="selectMulti" multiple onChange={this.handleCategoryChange}>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Input>
+                </FormGroup>
+                <Button color="primary" onClick={this.handleClick}>Create Relic</Button>
+            </Form>
         );
     }
 }
