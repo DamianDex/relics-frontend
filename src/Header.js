7328fa6c08c4866e4c84be204a67ea9616795802
piotrgryta <piotrek.gryta@gmail.com>
2018-03-12 21:44:05 +0100

    Removed unused imports from header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import CustomNavbar from './components/CustomNavbar'
 
-import { Switch, Route } from 'react-router-dom'
-import LoginPage from "./components/LoginPage";
-import App from "./App";
-
- 
 export default class Header extends React.Component {
   render() {
     return (
