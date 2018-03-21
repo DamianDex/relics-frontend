7578f764df8339eea6a6b5edd48878601b970d83
farad <damian.fara@pega.com>
2018-03-21 20:34:49 +0100

    Extend functionality of review component.

--- src/components/profiles/relic/reviews/ReviewComponent.js
+++ src/components/profiles/relic/reviews/ReviewComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Card, CardBody, CardHeader, CardText} from "reactstrap";
+import {Button, Card, CardBody, CardHeader, CardText, FormGroup, Input} from "reactstrap";
 import ReviewController from "../../../../controllers/ReviewController";
 
 export default class ReviewComponent extends Component {
@@ -9,31 +9,92 @@ export default class ReviewComponent extends Component {
         this.reviewController = new ReviewController();
 
         this.state = {
-            reviews: []
+            reviews: [],
+            isReviewed: false
         }
+
+        this.postReview = this.postReview.bind(this);
 
     }
 
     componentDidMount() {
         this.getRelicReviews();
+        this.checkIfUserReviewRelic();
     }
 
     render() {
+
+        if ((!this.state.isReviewed) && (this.state.reviews.length > 0)) {
+            return (
+                <div>
+                    {this.renderReviewForm()}
+                    {this.renderReviews()}
+                </div>
+            )
+        }
+
+        if (this.state.reviews.length > 0) {
+            return (
+                <div>
+                    {this.renderReviews()}
+                </div>
+            );
+        }
+
+        if (!this.state.isReviewed) {
+            return (
+                <div>
+                    {this.renderReviewForm()}
+                </div>
+            )
+        }
+
+    }
+
+    postReview() {
+        this.reviewController.postReview(1, 10, 10);
+    }
+
+    renderReviewForm() {
         return (
             <div>
-                {this.state.reviews.map((item, index) => (
+                <Card>
+                    <CardHeader>Czekamy na Twoją opinię!</CardHeader>
+                    <CardBody>
+                        <FormGroup>
+                            <Input type="select">
+                                <option>Wybierz swoją ocenę...</option>
+                                <option>Rewelacja (5)</option>
+                                <option>Bardzo fajne miejsce (4)</option>
+                                <option>Naprawdę warto (3)</option>
+                                <option>Tylko, gdy będziesz w pobliżu (2)</option>
+                                <option>Stanowczo odradzam (1)</option>
+                            </Input>
+                            <Input type="textarea" placeholder="Miejsce na Twój komentarz..." name="text"
+                                   id="exampleText"/>
+                        </FormGroup>
+                        <Button color="primary" onClick={this.postReview}>Zatwierdź ocenę!</Button>
+                    </CardBody>
+                </Card>
+                <br/>
+            </div>
+        );
+    }
+
+    renderReviews() {
+        return (
+            this.state.reviews.map((item, index) => (
+                <div>
                     <Card>
                         <CardHeader>Nazwa użytkownika</CardHeader>
                         <CardBody>
-                            <CardText>{index}</CardText>
-                            <p>{item.comment}</p>
-                            <p>{item.rating}</p>
-                            <CardText>Treść recenzji + ocena.</CardText>
+                            <CardText>Ocena: {item.rating} / 10</CardText>
+                            <p>Recenzja: {item.comment}</p>
                         </CardBody>
                     </Card>
-                ))}
-            </div>
-        );
+                    <br/>
+                </div>
+            )));
     }
 
     getRelicReviews() {
@@ -45,11 +106,25 @@ export default class ReviewComponent extends Component {
                         reviews: response.data
                     }
                 )
-                console.log(self.state.reviews);
             })
             .catch(error => {
                 console.log(error);
             })
+    }
+
+    checkIfUserReviewRelic() {
+        let self = this;
+        this.reviewController.checkIfUserReviewRelic(10, 10)
+            .then(response => {
+                self.setState(
+                    {
+                        isReviewed: false
+                    }
+                )
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
 }
