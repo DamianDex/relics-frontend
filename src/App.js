f3693c7ac791951b44800f2766354713e9ddb8f5
Sławek <sla.gladys@gmail.com>
2018-04-03 15:33:41 +0200

    Test for RelicAddPage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import LoginPage from "./components/LoginPage";
 import RelicsDBPage from "./components/RelicsDbPage";
 import RelicsLocalizePage from "./components/RelicsLocalizePage";
 import RelicsRankingPage from "./components/ranking/RelicsRankingPage";
-import RelicsAddPage from "./components/RelicsAddPage";
+import RelicAddPage from "./components/addRelic/RelicAddPage";
 import RelicProfilePage from "./components/profiles/relic/RelicProfilePage";
 import RegistrationPage from "./components/RegistrationPage";
 
@@ -23,7 +23,7 @@ export default class App extends Component {
                     <Route exact path='/relics' component={RelicsDBPage}/>
                     <Route exact path='/localize' component={RelicsLocalizePage}/>
                     <Route exact path='/ranking' component={RelicsRankingPage}/>
-                    <Route exact path='/add' component={RelicsAddPage}/>
+                    <Route exact path='/add' component={RelicAddPage}/>
                     <Route exact path='/relic/:relicId' component={RelicProfilePage}/>
                     <Route exact path='/register' component={RegistrationPage}/>
                 </Switch>
