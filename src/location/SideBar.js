431c14a55aa9eb9f2e1870c63bbca555bf5bafef
Piotrek <piotrek.gryta@gmail.com>
2018-06-05 20:19:14 +0200

    Localization page left side

--- src/location/SideBar.js
+++ src/location/SideBar.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { Card, CardSubtitle, CardBody, CardHeader, CardFooter, InputGroup, Input, Button, ListGroup, ListGroupItem} from 'reactstrap'
 
 
 export default class SideBar extends Component {
@@ -8,7 +9,58 @@ export default class SideBar extends Component {
 	
     render() {
         return (
-        	<div id="inner_remaining">
+        	<div id = "inner-remaining" className="sidebar">
+        		<CardHeader>
+        			Bufor odległości od trasy
+        		</CardHeader>
+        	    <InputGroup>
+        	      	<CardSubtitle className="buffer-text" >Wielkość bufora [km]: </CardSubtitle>
+        	       	<Input className="buffer-input" placeholder="Wpisz odległość od trasy..." type="number" step="1" min="0" />
+        	     </InputGroup>
+      	        <Button className="buffer-button" color="primary">Wyszukaj</Button>	
+        		<CardHeader>
+        			Odnalezione w buforze zabyki
+        		</CardHeader>
+        		<CardBody className="sidebar-list">
+        			<ListGroup>
+        				<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+            			<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+            			<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+            			<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+            			<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+            			<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+            				<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+            			<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+            			<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+            			<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+            			<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+            			<ListGroupItem>
+        					Pierwszy item
+        				</ListGroupItem>
+       				</ListGroup>
+   				</CardBody>
         	</div>
         );
     }
